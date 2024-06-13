b8cd0e1e9ea844fecfa8c252f21706f542b84fd2
falric05 <falco.riccardo@inaf.it>
2024-06-13 16:34:39 +0200

    Random number of peaks

--- gammasim/gammasim.py
+++ gammasim/gammasim.py
@@ -41,7 +41,7 @@ class GammaSim:
         return t_starts
 
 
-    def generate_dataset(self, F_saturation: bool) -> None:
+    def generate_dataset(self, F_saturation: bool, F_random_npeaks: bool = False) -> None:
         """
         Generate the dataset
         ## Args
@@ -51,7 +51,7 @@ class GammaSim:
         self.__dataset      = np.empty((self.size, self.xlen), dtype=np.int16)
         self.__labels_split = np.empty((self.size, self.max_peaks*self.xlen))
         self.__labels       = np.empty((self.size, self.xlen))
-        self.__integrals    = np.empty((self.size, self.max_peaks))
+        self.__integrals    = np.zeros((self.size, self.max_peaks))
         # Background of the signal
         x_base = self.bkgbase_level * np.ones_like(self.__t)
         # Select gamma_min and gamma_max depending on the saturation flag
@@ -60,9 +60,10 @@ class GammaSim:
             peak_params        = []
             peak_signals       = []
             peak_integrals     = []
+            m = int(np.random.uniform(1, self.max_peaks, 1)) if F_random_npeaks else self.max_peaks
             # Generate the tstart in a safe mode from overlaps
-            tstarts = self.__generate_tstarts(self.max_peaks)
-            for j in range(self.max_peaks):
+            tstarts = self.__generate_tstarts(m)
+            for j in range(m):
                 # Generate the parameters
                 gamma   = np.random.randint(gamma_min, gamma_max) 
                 tau1    = np.random.randint(self.tau1_min, self.tau1_max)
@@ -86,7 +87,7 @@ class GammaSim:
             # Apply Gauss noise 
             x_total_noise = exp.apply_gauss_round(x_total, self.gauss_mean, self.gauss_std)
             # integrals
-            self.__integrals[i][:self.max_peaks] = peak_integrals
+            self.__integrals[i][:m] = peak_integrals
             # Append dataset
             self.__dataset[i] = x_total_noise
             # Combine labels
